fcdd9948931bcb02ec34f261aba63ad2eba01f3a
Kaidi1 <winson096@gmail.com>
2022-09-18 21:33:01 -0400

    added flix files to repo

---
+++ Pods/Target Support Files/Pods-week 1-week 1UITests/Pods-week 1-week 1UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_week_1_week_1UITests : NSObject
+@end
+@implementation PodsDummy_Pods_week_1_week_1UITests
+@end
---
+++ week 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week 1
+//
+//  Created by Kaidi Gao on 9/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
