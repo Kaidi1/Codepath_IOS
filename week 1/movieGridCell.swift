a68b9b4195e51bb53407087953ec2885c1d0e1b6
Kaidi1 <winson096@gmail.com>
2022-09-23 20:16:38 -0400

    updated for flix part 2

---
+++ week 1/movieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  movieGridCell.swift
+//  week 1
+//
+//  Created by Kaidi Gao on 9/23/22.
+//
+
+import UIKit
+
+class movieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
